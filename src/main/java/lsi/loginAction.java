b8b72b956f2ac9bd1de3b27e7442f3e2c493b590
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-05-15 21:32:38 +0300

    add new components

---
+++ src/main/java/lsi/loginAction.java
@@ -0,0 +1,24 @@
+package lsi;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class loginAction extends Action {
+
+	public ActionForward execute(
+		ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws Exception {
+                       loginForm inputForm = (loginForm) form;
+			System.out.println(inputForm.getUsername());
+			return mapping.findForward("success");
+	}
+
+}
